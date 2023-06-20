8644673fe6a2a83e29a7f58dc8e1f30f7e4caf4e
Nortin Powers <a.nortin@gmail.com>
2023-06-20 19:45:12 +0300

    bug fixes and improvements: fix tests

--- src/test/java/by/tms/eshop/service/ShopFacadeTest.java
+++ src/test/java/by/tms/eshop/service/ShopFacadeTest.java
@@ -28,6 +28,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 @SpringBootTest
 class ShopFacadeTest {
 
+    private final Long productId = 1L;
+    private final String shopFlagElse = "someFlag";
+    private final String location = "someLocation";
+    private final Integer page = 2;
+    private final String extensionPath = AND_PAGE + page;
+
     @Autowired
     private ShopFacade shopFacade;
 
@@ -38,21 +44,32 @@ class ShopFacadeTest {
     private CartService cartService;
 
     @Test
-    void getPathFromAddCartByParameters() {
-        Long productId = 1L;
+    void getPathFromAddCartByParametersToCart() {
         String shopFlagTrue = TRUE;
-        String shopFlagElse = "someFlag";
-        String location = "someLocation";
-        Integer page = 2;
-        String extensionPath = AND_PAGE + page;
         Assertions.assertEquals(REDIRECT_TO_CART, shopFacade.getPathFromAddCartByParameters(productId, shopFlagTrue, location, null));
         Assertions.assertEquals(REDIRECT_TO_CART + extensionPath, shopFacade.getPathFromAddCartByParameters(productId, shopFlagTrue, location, page));
+    }
+
+    @Test
+    void getPathFromAddCartByParametersToFavorites() {
         Assertions.assertEquals(REDIRECT_TO_FAVORITES, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, FAVORITE, null));
         Assertions.assertEquals(REDIRECT_TO_FAVORITES + extensionPath, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, FAVORITE, page));
+    }
+
+    @Test
+    void getPathFromAddCartByParametersToSearch() {
         Assertions.assertEquals(REDIRECT_TO_SEARCH_RESULT_SAVE, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, SEARCH, null));
         Assertions.assertEquals(REDIRECT_TO_SEARCH_RESULT_SAVE + extensionPath, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, SEARCH, page));
+    }
+
+    @Test
+    void getPathFromAddCartByParametersToProduct() {
         Assertions.assertEquals(REDIRECT_TO_PRODUCT_WITH_PARAM + productId, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, PRODUCT_PAGE, null));
         Assertions.assertEquals(REDIRECT_TO_PRODUCT_WITH_PARAM + productId + extensionPath, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, PRODUCT_PAGE, page));
+    }
+
+    @Test
+    void getPathFromAddCartByParametersToProducts() {
         String productCategory = "someCategory";
         Mockito.when(productService.getProductCategoryValue(productId)).thenReturn(productCategory);
         Assertions.assertEquals(REDIRECT_TO_PRODUCTS_PAGE_CATEGORY_WITH_PARAM + productCategory + AND_SIZE + SIZE_3, shopFacade.getPathFromAddCartByParameters(productId, shopFlagElse, location, null));
@@ -71,9 +88,9 @@ class ShopFacadeTest {
         List<CartDto> cartDtos = List.of(CartDto.builder()
                                                 .id(2L)
                                                 .productDto(productDto)
-                                                 .cart(false)
-                                                 .favorite(true)
-                                                 .count(3)
+                                                .cart(false)
+                                                .favorite(true)
+                                                .count(3)
                                                 .build());
         Long userId = 4L;
         Mockito.when(cartService.getSelectedProducts(userId, Location.FAVORITE)).thenReturn(cartDtos);
--- src/test/java/by/tms/eshop/utils/ControllerUtilsTest.java
+++ src/test/java/by/tms/eshop/utils/ControllerUtilsTest.java
@@ -20,17 +20,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ControllerUtilsTest {
 
+    private final Long productId = 1L;
+    private final Integer page = 2;
+    private final String extensionPath = AND_PAGE + page;
+    private final String productCategory = "someCategory";
+
     @Test
-    void getPathFromAddFavoriteByParameters() {
-        Long productId = 1L;
-        Integer page = 2;
-        String extensionPath = AND_PAGE + page;
-        String location = "someLocation";
-        String productCategory = "someCategory";
+    void getPathFromAddFavoriteByParametersToSearch() {
         Assertions.assertEquals(REDIRECT_TO_SEARCH_RESULT_SAVE, ControllerUtils.getPathFromAddFavoriteByParameters(productId, SEARCH, productCategory, null));
         Assertions.assertEquals(REDIRECT_TO_SEARCH_RESULT_SAVE + extensionPath, ControllerUtils.getPathFromAddFavoriteByParameters(productId, SEARCH, productCategory, page));
+    }
+
+    @Test
+    void getPathFromAddFavoriteByParametersToProduct() {
         Assertions.assertEquals(REDIRECT_TO_PRODUCT_WITH_PARAM + productId, ControllerUtils.getPathFromAddFavoriteByParameters(productId, PRODUCT_PAGE, productCategory, null));
         Assertions.assertEquals(REDIRECT_TO_PRODUCT_WITH_PARAM + productId + extensionPath, ControllerUtils.getPathFromAddFavoriteByParameters(productId, PRODUCT_PAGE, productCategory, page));
+    }
+
+    @Test
+    void getPathFromAddFavoriteByParametersToProducts() {
+        String location = "someLocation";
         Assertions.assertEquals(REDIRECT_TO_PRODUCTS_PAGE_CATEGORY_WITH_PARAM + productCategory + AND_SIZE + SIZE_3, ControllerUtils.getPathFromAddFavoriteByParameters(productId, location, productCategory, null));
         Assertions.assertEquals(REDIRECT_TO_PRODUCTS_PAGE_CATEGORY_WITH_PARAM + productCategory + AND_SIZE + SIZE_3 + extensionPath, ControllerUtils.getPathFromAddFavoriteByParameters(productId, location, productCategory, page));
     }
