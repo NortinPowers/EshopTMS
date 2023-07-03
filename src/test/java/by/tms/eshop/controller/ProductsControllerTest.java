b751e3a7bad5252c38a992a66c3758d2ea68ea0c
Nortin Powers <a.nortin@gmail.com>
2023-07-03 17:17:21 +0300

    created tests for ProductsControllerTest and fix html - checking for null

--- src/test/java/by/tms/eshop/controller/ProductsControllerTest.java
+++ src/test/java/by/tms/eshop/controller/ProductsControllerTest.java
@@ -1,8 +1,10 @@
 package by.tms.eshop.controller;
 
+import static by.tms.eshop.test_utils.Constants.PRODUCT_ID;
 import static by.tms.eshop.test_utils.Constants.TV;
 import static by.tms.eshop.utils.Constants.CATEGORY;
 import static by.tms.eshop.utils.Constants.ControllerMappingPath.ESHOP;
+import static by.tms.eshop.utils.Constants.MappingPath.PRODUCT;
 import static by.tms.eshop.utils.Constants.MappingPath.REDIRECT_TO_ESHOP;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -10,6 +12,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import by.tms.eshop.service.ProductService;
 import org.junit.jupiter.api.Nested;
@@ -34,24 +37,27 @@ class ProductsControllerTest {
     @MockBean
     private ProductService productService;
 
+    private ModelAndView expectedModelAndView;
+    private Long productId;
+
     @Nested
     class TestShowProductsPage {
 
         @Test
         @WithAnonymousUser
         void test_showProductsPage_anonymousUser() throws Exception {
-            inspectShowProductPage();
+            inspectShowProductsPage();
         }
 
         @Test
         @WithMockUser(roles = {"USER", "ADMIN"})
         void test_showProductsPage_userWithRole() throws Exception {
-            inspectShowProductPage();
+            inspectShowProductsPage();
         }
 
-        private void inspectShowProductPage() throws Exception {
+        private void inspectShowProductsPage() throws Exception {
             String category = TV;
-            ModelAndView expectedModelAndView = new ModelAndView(REDIRECT_TO_ESHOP);
+            expectedModelAndView = new ModelAndView(REDIRECT_TO_ESHOP);
 
             when(productService.getViewProductsByCategory(eq(category), any(Pageable.class))).thenReturn(expectedModelAndView);
 
@@ -64,7 +70,47 @@ class ProductsControllerTest {
         }
     }
 
-    @Test
-    void showProductPage() {
+    @Nested
+    class TestShowProductPage {
+
+        @Test
+        @WithAnonymousUser
+        void test_showProductPage_anonymousUser_withPathVariable() throws Exception {
+            inspectShowProductPageWithPathVariable();
+        }
+
+        @Test
+        @WithMockUser(roles = {"USER", "ADMIN"})
+        void test_showProductPage_userWithRole_withPathVariable() throws Exception {
+            inspectShowProductPageWithPathVariable();
+        }
+
+        @Test
+        @WithAnonymousUser
+        void test_showProductPage_anonymousUser_withoutPathVariable() throws Exception {
+            inspectShowProductPageWithoutPathVariable();
+        }
+
+        @Test
+        @WithMockUser(roles = {"USER", "ADMIN"})
+        void test_showProductPage_userWithRole_withoutPathVariable() throws Exception {
+            inspectShowProductPageWithoutPathVariable();
+        }
+
+        private void inspectShowProductPageWithPathVariable() throws Exception {
+            productId = PRODUCT_ID;
+            expectedModelAndView = new ModelAndView(PRODUCT);
+
+            when(productService.getViewProduct(productId)).thenReturn(expectedModelAndView);
+
+            mockMvc.perform(get("/product/{id}", productId))
+                   .andExpect(status().isOk())
+                   .andExpect(view().name(PRODUCT));
+        }
+
+        private void inspectShowProductPageWithoutPathVariable() throws Exception {
+            mockMvc.perform(get("/product/{id}", productId))
+                   .andExpect(status().is4xxClientError());
+        }
     }
 }
